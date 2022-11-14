04771cc9894649f27df4d7ae866677243ad8a5a0
chrsippl <95415319+chrsippl@users.noreply.github.com>
2022-11-14 15:03:46 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MIELSTONE'
+copyright = '2022'
+author = 'CS'
 
 release = '0.1'
 version = '0.1.0'
